ef7d2864c43ef49fed7e19dc34419c0203c76072
rainchill <rainchill2022@foxmail.com>
2023-11-19 19:49:48 +0800

    多设备同步代码

--- server.js
+++ server.js
@@ -13,9 +13,33 @@ var storage = multer.diskStorage({
     // 文件重命名
     filename: function (req, file, cb) {
       console.log(file)
-      cb(null,  file.originalname);
+        //   cb(null,  file.originalname);
+      cb(null,  Buffer.from(file.originalname, "latin1").toString("utf8"));
       // cb(null, file.fieldname + '.jpg');
-    }
+    },
+    // filename: function(req, file, cb) {
+    //     // const fileFormat = (file.originalname).split('.') // 取后缀
+    //     // let fileName = file.fieldname + '-' + Date.now() + ext;
+    //     // 解决中文名乱码的问题
+    //     file.originalname = Buffer.from(file.originalname, "latin1").toString("utf8")
+    //     let fileName = file.originalname
+    //     // 判断文件是否存在
+    //     // fs.access(fileName, error => {
+    //     //     if (!error) {
+    //     //         // The check succeeded
+    //     //         //fileName就是上传文件的文件名
+    //     //         cb(null, fileName);
+    //     //     } else {
+    //     //         // The check failed
+    //     //         let ext = path.extname(file.originalname);
+    //     //         let basename = path.basename(file.originalname)
+    //     //         let str = basename.split("."); // 以‘.’分割；
+    //     //         fileName = str[0] + '-' + Date.now() + ext;
+    //     //         //fileName就是上传文件的文件名
+    //     //         cb(null, fileName);
+    //     //     }
+    //     // })
+    // }
   })
 const upload = multer({ storage: storage })
 // const upload = multer({ dest: path.join(__dirname, '/upload') })
@@ -110,13 +134,13 @@ app.get('/articles', function (req, res) {
             )
         }
         obj.articleslist.reverse()
-        console.log("obj:", obj)
+        // console.log("obj:", obj)
     }
 
     x = { articleslist:[] }
     sendArticles(x).then(() => {
-        console.log("2221111:", x)
-        console.log("send json:", JSON.stringify(x.articleslist))
+        // console.log("2221111:", x)
+        // console.log("send json:", JSON.stringify(x.articleslist))
         res.send(JSON.stringify(x.articleslist))
     })
 
@@ -136,7 +160,7 @@ app.get('/articles', function (req, res) {
 })
 
 app.get('/articles/:id', function (req, res) {
-    console.log("send page")
+    // console.log("send page")
     // console.log("id=",req.params.id)
     
 
@@ -151,9 +175,9 @@ app.get('/articles/:id', function (req, res) {
         if(article === null) {
             return
         }
-        console.log("aaa=",article)
+        // console.log("aaa=",article)
         obj.article = {"title": article.dataValues.ztitle.split(".")[0], "content": article.dataValues.zcontent}
-        console.log("ooobj=",obj)
+        // console.log("ooobj=",obj)
     }
 
     seeArticle(x).then(() => {
@@ -163,22 +187,37 @@ app.get('/articles/:id', function (req, res) {
 
 app.post('/api/admin/uploadfile',  upload.single('file'), function (req, res) {
     res.send({ file: "ok" })
-
-    if("md" === req.file.filename.split(".")[1]) {
+    // filename = Buffer.from(req.file.filename, "latin1").toString("utf8");
+    if ("md" === req.file.filename.split(".")[1]) {
         fs.readFile("./upload/" + req.file.filename, function(err, data) {
             if (err) {
                 return console.error(err)
             }
-            console.log("异步读取:", data.toString())
+            // console.log("异步读取:", data.toString())
             insert_sql = (async () => {
                 console.log("insert sql")
                 await Articles.create({
-                    ztitle: req.file.originalname,
+                    ztitle: req.file.filename,
                     zcontent: data.toString()
                 })
             })()
         })
     }
+})
+
+app.post('/api/admin/delete', function (req, res) {
+    console.log("-------", req.body.deleteList)
+    var deleteList = req.body.deleteList.map(v => v+'.md')
+    for (const v of deleteList) {
+        (async () => {
+            await Articles.destroy({
+                where: {
+                    ztitle: v
+                }
+            })
+        }) ()
+    }
+    res.send({ code: "ok" })
 })
 
 
@@ -200,7 +239,7 @@ app.get('/api/admin/articleslist', function (req, res) {
 
     x = { articleslist:[] }
     getArticleslist(x).then(() => {
-        console.log("x =", x.articleslist)
+        // console.log("x =", x.articleslist)
         res.send(JSON.stringify(x.articleslist))
     })
 
